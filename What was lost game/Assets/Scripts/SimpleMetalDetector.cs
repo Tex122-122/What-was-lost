59ed8eef6d4009d95c462359ca10640e04d48878
David Strudwick <david.strudwick1301@gmail.com>
2021-02-16 17:52:32 +1100

    First Prototype
    Distance detection

---
+++ What was lost game/Assets/Scripts/SimpleMetalDetector.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class SimpleMetalDetector : MonoBehaviour
+{
+    //Distance detection
+    public Transform target;
+    private float distance;
+    //Score counting
+    public Text myText;
+    private int score = 0;
+    void Update()
+    {
+        //tracks distance between player and object, triggering a Collect when the player walks over the object
+        distance = Vector3.Distance(transform.position, target.position);
+        print("distance = " + distance);
+        myText.text = score.ToString() + "   " + (Mathf.Round(distance)).ToString();
+        if (distance < 2)
+        {
+            Collect();
+        }
+    }
+    void Collect()
+    {
+        ++score;
+        
+        //Randomize location
+        Vector3 pos = transform.position;
+
+        pos.x = Random.Range(-45.0f, 45.0f);
+        pos.y = 0;
+        pos.z = Random.Range(-45.0f, 45.0f);
+
+        transform.position = pos;
+    }
+}
---
+++ What was lost game/Assets/Scripts/playerController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+
+{
+    
+    private float speed = 10f;
+
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        transform.position = transform.position + new Vector3(horizontalInput * speed * Time.deltaTime, 0, verticalInput * speed * Time.deltaTime);
+    }
+}
+
